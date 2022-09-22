833e930261b44d6ddac4598374238dd9aa67f007
Reymon <you@example.com>
2022-09-22 06:11:50 -0300

    06:011 22/09

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -108,7 +108,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ((num % 2) = 0) {
+  if (num % 2 = 0) {
     return true;
   }
   return false;

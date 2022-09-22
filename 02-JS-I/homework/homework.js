96a986504ee35947aef6d7425e4e170836746c4d
Reymon <you@example.com>
2022-09-22 00:08:52 -0300

    homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -118,7 +118,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ((num % 2) > 0) {
+  if (num % 2 > 0) {
     return true;
   }
   return false;
